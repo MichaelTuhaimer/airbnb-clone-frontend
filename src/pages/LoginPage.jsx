03b84f2e9480758d59c90be4ec21cfdbfd3656d9
merrelltd72 <tyron.merrell@gmail.com>
2024-08-08 12:26:12 -0500

    Implemented login and logout functionality.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,60 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+}
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [errors, setErrors] = useState([]);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    const params = new FormData(e.target);
+    axios
+      .post("http://localhost:3000/sessions.json", params)
+      .then((res) => {
+        console.log(res.data);
+        axios.defaults.headers.common["Authorization"] =
+          "Bearer" + res.data.jwt;
+        localStorage.setItem("jwt", res.data.jwt);
+        e.target.reset();
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.response);
+        setErrors(["invalid email and/or password"]);
+      });
+  };
+
+  return (
+    <>
+      <div id="login">
+        <h1>Login</h1>
+        <ul>
+          {errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Login Form</legend>
+            <div>
+              Email: <input name="email" type="email" />
+            </div>
+            <div>
+              Password: <input name="password" type="password" />
+            </div>
+            <button type="submit">Login</button>
+          </fieldset>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
